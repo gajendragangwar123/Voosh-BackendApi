66cf16c34637739ec34568cf167a3075b26846a5
gajendragangwar123 <118338797+gajendragangwar123@users.noreply.github.com>
2023-02-09 10:33:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.mongodb://fullstack-app-server:6DEK6Lw5CpNez5aXmPrMicvqYKoVFVmtK6eRMbxvxIwVflGKCcaRXGtX8vvk3iaZvjzeeb37gr4pACDbNw8sng==@fullstack-app-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@fullstack-app-server@);
+mongoose.connect(process.env.db_url);
 const db = mongoose.connection;
 
 db.on("error" , console.error.bind(console , "Error while connecting with the database "));
